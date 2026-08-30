c99381536a02ca37f1e85b90d0079de0cb7cb4ed
Trim Bresilla <trim.bresilla@gmail.com>
2026-08-30 21:28:25 +0200

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+//! aeon.
+
+/// Returns this crate's display name.
+pub fn name() -> &'static str {
+    "aeon"
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn exposes_name() {
+        assert_eq!(name(), "aeon");
+    }
+}
